0cc60a564a4635569420bec741d74a88c0aac2e9
devops <devops23345@gmail.com>
2019-12-17 22:06:15 -0500

    Added more functionality to pull all exhibitors from list

--- ces_exhibitors.py
+++ ces_exhibitors.py
@@ -1,33 +1,42 @@
 import os
 print("Current File Name : ",os.path.realpath(__file__))
 
-
 import requests
 
 #ces_url = 'https://www.ces.tech/api/Exhibitors?searchTerm=&sortBy=alpha&filter=A&pageNo=1&pageSize=15'
 ces_base_url = 'https://www.ces.tech/api/Exhibitors?searchTerm=&sortBy=alpha'
-param1_base = '&filter=A'
+param1_base = '&filter='
 param2_base = '&pageNo=1'
 param3_base = '&pageSize=15'
 
+URL_HEADER = {'ctaapi-version': '1.1'}
+
+EXHIBITOR_SEARCH_FILTER = ['#', 'A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V',
+                           'W','X','Y','Z']
 
 
-ces_url_full = ces_base_url+param1_base+param2_base+param3_base
+for f1 in EXHIBITOR_SEARCH_FILTER:
+    param1_full = param1_base + f1
+    ces_url_full = ces_base_url+param1_full+param2_base+param3_base
+    print(ces_url_full)
+    r = requests.get(ces_url_full, headers = URL_HEADER)
+    ces_response_json = r.json()
 
-ces_url_header = {'ctaapi-version': '1.1'}
+    for p in ces_response_json['exhibitors']:
+        print('Company Name:: ', p['companyName'], ' Description:: ', p['description'])
 
-r = requests.get(ces_url_full, headers = ces_url_header)
+###
 #print out the url
-print('URL Request::', r.url)
-
-print('Request Headers')
-print(r.request.headers)
-
-ces_response_json = r.json()
-
-for p in ces_response_json['exhibitors']:
-    print(p['companyName'])
-
+# print('URL Request::', r.url)
+#
+# print('Request Headers')
+# print(r.request.headers)
+#
+# ces_response_json = r.json()
+#
+# for p in ces_response_json['exhibitors']:
+#     print(p['companyName'])
+###
 
 
 
